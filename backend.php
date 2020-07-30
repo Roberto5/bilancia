b3f6ed64fd1542136a4452aabd17df6f1c5316c5
roberto5 <hitmanghe@gmail.com>
2020-07-30 14:20:03 +0200

    add conf file and better debug input

--- backend.php
+++ backend.php
@@ -3,6 +3,16 @@ include 'handler.php';
 
 //debug mode
 $handler=null;//new debugReader();
-$reader=new handler($handler);
+$option=array();
+if (isset($_GET['setZero'])&& $_GET['setZero']) {
+    $option['zero']=true;
+}
+if (isset($_GET['history']) && $_GET['history']) {
+    $option['history']=true;
+}
+echo 'option';
+print_r($option);
+echo'<br>';
+$reader=new handler($handler,$option);
 
 echo json_encode($reader->read());
--- handler.php
+++ handler.php
@@ -3,18 +3,66 @@
 
 
 class handler {
+    private $file='db.json';
     var $reader=null;
-    function __construct($handler) {
+    var $zero=null;
+    var $history=null;
+    var $buffer=false;
+    function __construct($handler,$config=array()) {
         if ($handler instanceof readerHandelr)
             $this->reader=$handler;
         else $this->reader=new debugReader();
+        
+        $conf=$this->readConf();
+        print_r($conf);
+        $write=false;
+        
+        $default=array(
+            'zero'=>false,
+            'history'=>false,
+        );
+        
+        foreach ($default as $key => $value) {
+            if (isset($config[$key])) {
+                echo $key;
+                $value=$config[$key];
+            }
+            if (($key=='zero')&& $value) {
+                echo 'zero!';
+                $this->buffer=$this->read();
+                $value=$this->buffer['weight'];
+                $this->buffer['weight']=0;
+                $conf['zero']=$value;
+                $write=true;
+            }
+            $this->$key=$value;
+        }
+        if (isset($conf['zero'])) $this->zero=$conf['zero'];
+        if ($write) {
+            $this->writeConf($conf);
+        }
+        
+        
+        
+    }
+    function readConf() {
+        $conf=file_get_contents($this->file);
+        return json_decode($conf,true);
+    }
+    function writeConf($conf) {
+        file_put_contents($this->file, json_encode($conf));
     }
     /**
      * read inpud 
      * @return array
      */
     function read() {
-        return $this->reader->read();
+        if ($this->buffer) $result=$this->buffer;
+        else {
+            $result=$this->reader->read();
+            $result['weight']-=$this->zero;
+        }
+        return $result;
     }
 }
 
